30bb07510eb58e7b829e783b4bcafb56a2450194
Frederick Mfinanga <frederickmfinanga@Fredericks-MBP.local>
2018-05-25 02:27:03 -0400

    would your rather

---
+++ src/components/404.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFoundPage extends Component {
+    render() {
+        return (
+            <div style={{ textAlign: 'center', marginTop: '50px' }}>
+                The page you're looking for doesn't exists.
+            </div>
+        )
+    }
+}
+
+export default NotFoundPage;
